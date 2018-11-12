7e3f8eb0ea9a63dc57c66401e293aa927535b517
sc2ad <adamznow@gmail.com>
2018-11-11 21:03:48 -0800

    Init Commit

---
+++ DustDevilPatch/Patches/Hooks/patch_Object.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DustDevil;
+
+namespace UnityEngine.mm.Patches.Hooks {
+    [MonoMod.MonoModPatch( "global::UnityEngine.Object" )]
+    class patch_Object: Object {
+
+        [MonoMod.MonoModIgnore]
+        public static int OffsetOfInstanceIDInCPlusPlusObject;
+
+        [MonoMod.MonoModConstructor]
+        public static void cctor_Object() {
+            OffsetOfInstanceIDInCPlusPlusObject = -1;
+
+            Debug.Log( "Let's hook some code!" );
+            try {
+                DustDevilManager.CreateInstance();
+            } catch( System.Exception e ) {
+                Debug.LogError(e);
+                File.WriteAllText( Application.dataPath + "/errorPartiality.txt", e.ToString() );
+            }
+        }
+    }
+}
